f7da9483a399fa8df792bea9f58d00374a8da9bb
Lookofsea <919189565@qq.com>
2024-08-04 23:19:29 +0800

    COMMIT

---
+++ Python_basic/Lists.py
@@ -0,0 +1,16 @@
+# write a program to remove the duplicates in a list
+
+# numbers = [2,2,5,5,6,9,8,1]
+# numbers_no_duplicate = []
+# for number in numbers:
+#     if number not in numbers_no_duplicate:
+#         numbers_no_duplicate.append(number)
+
+# print(numbers_no_duplicate)
+
+def greet(first,last):
+    print(f'{first} {last}')
+
+greet(first='mark',last = 'mary')
+
+
